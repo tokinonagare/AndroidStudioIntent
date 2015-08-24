c71b9e7ed0c97b69c042777b7e75571b05773982
tokinonagare <timemahou@gmail.com>
2015-08-24 17:25:50 +0800

    Intent one activity to another

---
+++ app/src/main/java/com/tokinonagare/www/intentexample/Bacon.java
@@ -0,0 +1,46 @@
+package com.tokinonagare.www.intentexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Bacon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacon);
+
+        Bundle applesData = getIntent().getExtras();
+        if (applesData==null){
+            return;
+        }
+        String appleMessage = applesData.getString("appleMessage");
+        final TextView baconText = (TextView) findViewById(R.id.baconText);
+        baconText.setText(appleMessage);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bacon, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
